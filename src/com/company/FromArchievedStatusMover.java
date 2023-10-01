1ebaf89eb5dc06fea833a57c9346bb18c736fcbf
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2023-10-01 12:13:29 +0300

    + switch

--- src/com/company/FromArchievedStatusMover.java
+++ src/com/company/FromArchievedStatusMover.java
@@ -11,13 +11,11 @@ public class FromArchievedStatusMover extends BookMover{
 //        }
 
         if(book.getStatus() == Status.ARCHIVED){
-            switch (requestedStatus){
-                case AVAILABLE:
-                    book.setStatus(requestedStatus);
-                    System.out.println("Книга переведена в статус - " + book.getStatus());
-                    break;
-                default:
-                    System.out.println("Книга уже находится в этом статусе или перевод возможен только в AVAILABLE!");
+            if (requestedStatus == Status.AVAILABLE) {
+                book.setStatus(requestedStatus);
+                System.out.println("Книга переведена в статус - " + book.getStatus());
+            } else {
+                System.out.println("Книга уже находится в этом статусе или перевод возможен только в AVAILABLE!");
             }
         }
     }
